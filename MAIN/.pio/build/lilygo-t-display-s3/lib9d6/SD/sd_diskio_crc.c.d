b96a00885084846e245bc1591ffe6822db1493f9
pandekager8 <johannes@vad-mortensen.dk>
2026-04-14 14:39:18 +0200

    ryddet op efter marcus1

---
+++ MAIN/.pio/build/lilygo-t-display-s3/lib9d6/SD/sd_diskio_crc.c.d
@@ -0,0 +1,2 @@
+.pio/build/lilygo-t-display-s3/lib9d6/SD/sd_diskio_crc.c.o: \
+ C:/Users/johan/.platformio/packages/framework-arduinoespressif32/libraries/SD/src/sd_diskio_crc.c
---
+++ MAIN/.pio/build/lilygo-t-display-s3/libdad/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/lilygo-t-display-s3/libdad/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/johan/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/johan/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/johan/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
